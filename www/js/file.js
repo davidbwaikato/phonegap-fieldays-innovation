85e824219c34411e417e1f55dd9ae1b5868ea4e1
David Bainbridge <davidb@cs.waikato.ac.nz>
2014-04-03 11:40:43 +1300

    Further phonegap file testing

--- www/js/file.js
+++ www/js/file.js
@@ -1,10 +1,14 @@
 document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady() {
+    console.error("Device is ready!");
     window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
 }
 
 function gotFS(fileSystem) {
+    console.error("Filesystem name = " + fileSystem.name);
+    console.error("Filesystem root name = " + fileSystem.root.fullPath);
+
     fileSystem.root.getFile("readme.txt", null, gotFileEntry, fail);
 }
 
