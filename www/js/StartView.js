8f0848b156ca661220a990495761348e8ae107fa
David Bainbridge <davidb@cs.waikato.ac.nz>
2014-04-04 11:46:41 +1300

    Initial stage of adding in scanner

--- www/js/StartView.js
+++ www/js/StartView.js
@@ -7,6 +7,9 @@ var StartView = function(homeView) {
     this.initialize = function() {
         // 'div' wrapper to attach html and events to
         this.el = $('<div/>');
+
+	this.el.on('click', '.scanner-btn', this.scan);
+
     };
 
     this.render = function() {
@@ -23,7 +26,35 @@ var StartView = function(homeView) {
 	$("#after-bubbles").delay(20000).animate({ opacity: 1 }, 700);
     };
 
+
+
+    this.scan = function() {
+        console.log('scan(): init');
+        // documentation said the syntax was this:
+        // var scanner = window.PhoneGap.require("cordova/plugin/BarcodeScanner");
+        // but playing with options, seems like it should be this:
+        //var scanner = window.cordova.require("cordova/plugin/BarcodeScanner");
+
+	cordova.plugins.barcodeScanner.scan(
+                function (result) {
+                    app.showAlert("Result: " + result.text + "\n"
+				  + "Format: " + result.format + "\n"
+				  + "Cancelled: " + result.cancelled,
+				  "We got a barcode");
+                },
+                function (error) {
+                    app.showAlert(err,"Scanning failed: ");
+                }
+        );
+    };
+
     this.initialize();
+
+
+
+
+
+
 
 }
 
